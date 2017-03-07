4fa8db7cdc6320fc0c572c16e9759f790e7891ed
foliwe <foliwe@gmail.com>
2017-03-07 00:12:45 +0000

    Implemented Concern into Models for managing image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306215631) do
+ActiveRecord::Schema.define(version: 20170306234915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170306215631) do
     t.integer  "percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
