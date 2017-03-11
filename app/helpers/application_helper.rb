b5e23851be065749de49352f008805ab21d419ee
foliwe <foliwe@gmail.com>
2017-03-11 19:02:23 +0000

    Craeted View Helper for Auth Links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+
+  def login_in_helper
+    if current_user.is_a?(User) #User here is the Super class for the User model
+    link_to 'Logout', destroy_user_session_path, method: :delete 
+    else
+    (link_to 'Register',  new_user_registration_path ) + 
+    "<br>".html_safe +
+    (link_to 'Login',  new_user_session_path)
+  end
+  end
 end
 
