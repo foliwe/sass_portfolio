2cf9af6e2fdc80e85d99f645be95563ae8bf9e71
foliwe <foliwe@gmail.com>
2017-03-12 21:42:33 +0000

    Finished Styling Of Devise Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,19 @@
 module ApplicationHelper
 
-  def login_in_helper
+  def login_in_helper style
     if current_user.is_a?(GuestUser) #User here is the Super class for the User model
-     ( link_to 'Login',  new_user_session_path) +
-      "<br>".html_safe +
-      (link_to 'Register',  new_user_registration_path )
+     ( link_to 'Login',  new_user_session_path, class: style) +
+      "".html_safe +
+      (link_to 'Register',  new_user_registration_path , class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete 
+      link_to 'Logout', destroy_user_session_path, method: :delete , class: style
     end
   end
 
-  def source_helper
-     if session[:source]
-    content_tag(:p, "Thanks for visiting me from #{session[:source]}",class:"session-source")
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
   
